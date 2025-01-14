08e9131250fe637619c0fec81fc3a9008cb01e95
Mohankrishnashrestha <shrestharajan0077@gmail.com>
2025-01-14 22:13:40 +0545

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
-import Gallery from "./pages/gallery/Gallery";
 import Header from "./component/header/Header";
+
+import Portfolio from "./pages/portfolio/Portfolio";
+import Services from "./pages/servicess/Services";
+import Contact from "./pages/contact/Contact";
 import { Route, Routes } from "react-router";
 import "./App.css";
 function App() {
@@ -11,8 +14,10 @@ function App() {
 
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/aboutUs" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/service" element={<Services />} />
+        <Route path="/contactUs" element={<Contact />} />
       </Routes>
     </div>
   );
--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -4,6 +4,7 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa6";
 import { FaLinkedinIn } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { FaHeadphonesSimple } from "react-icons/fa6";
 
 import "./header.css";
 import { useState } from "react";
@@ -11,35 +12,65 @@ function Header() {
   const [change, setChange] = useState(false);
 
   return (
-    <div className="main">
-      <div className="heading">
-        <ul className="header">
-          <li>
-            <FaFacebookF />
-          </li>
-          <li>
-            <FaTwitter />
-          </li>
-          <li>
-            <FaLinkedinIn />
-          </li>
-          <li>
-            <FaInstagram />
-          </li>
-        </ul>
-      </div>
-      <div className="titleAndHamburger">
-        <div className="title">Subha Bihani Digital Media Pvt. Ltd</div>
-        <div className="hamburger" onClick={() => setChange(!change)}>
-          <GiHamburgerMenu />
+    <>
+      <div className="main">
+        <div className="heading">
+          <ul className="header">
+            <li>
+              <FaFacebookF />
+            </li>
+            <li>
+              <FaTwitter />
+            </li>
+            <li>
+              <FaLinkedinIn />
+            </li>
+            <li>
+              <FaInstagram />
+            </li>
+          </ul>
+        </div>
+        <div className="titleAndHamburger">
+          <div className="title">Subha Bihani Digital Media Pvt. Ltd</div>
+          <div className="hamburger" onClick={() => setChange(!change)}>
+            <GiHamburgerMenu />
+          </div>
+        </div>
+        <div className={change ? "open" : "navicon"}>
+          <NavLink to={"/home"}>Home</NavLink>
+          <NavLink to={"/aboutUs"}>About</NavLink>
+          <NavLink to={"/portfolio"}>Portfolio</NavLink>
+          <NavLink to={"/service"}>Services</NavLink>
+          <NavLink to={"/contactUs"}>Contact</NavLink>
         </div>
       </div>
-      <div className={change ? "open" : "navicon"}>
-        <NavLink to={"/home"}>Home</NavLink>
-        <NavLink to={"/about"}>About</NavLink>
-        <NavLink to={"/gallery"}>Gallery</NavLink>
+      <div className="main2">
+        <div className="heading">
+          <div className="heading1">
+            <div>
+              <FaHeadphonesSimple />
+            </div>
+            <div>24 x 7 Technical Support</div>
+          </div>
+          <div>
+            <ul className="header">
+              <li>
+                <FaFacebookF />
+              </li>
+              <li>
+                <FaTwitter />
+              </li>
+              <li>
+                <FaLinkedinIn />
+              </li>
+              <li>
+                <FaInstagram />
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
